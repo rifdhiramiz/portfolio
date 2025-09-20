2cc905889b8332e7f5f667acb0d61c758da22985
rifdhiramiz <rifdhiramiz18@gmail.com>
2025-09-20 19:58:20 +0530

    Fix MIME error and update portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/portfolio/', // 👈 must match your repo name
+  base: '/portfolio/', // <-- your repo name
+  plugins: [react()],
 })
