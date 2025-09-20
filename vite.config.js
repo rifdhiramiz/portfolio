7747871bab0bb967ca69277376dd63ff7ddf2bb5
rifdhiramiz <rifdhiramiz18@gmail.com>
2025-09-20 18:59:13 +0530

    Deploy portfolio to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   // Use / for local dev, /my_portfolio/ for production
-  base: process.env.NODE_ENV === "production" ? "/my_portfolio/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
   plugins: [
     react(),
     tailwindcss()
